71c70afff461f35a11f1b153de777cf2432a572c
imxande <alerodriguezlarosa@gmail.com>
2020-02-01 15:55:48 -0500

    get all users, create and user and delete user functional

--- users/users-router.js
+++ users/users-router.js
@@ -15,25 +15,42 @@ router.get('/', restricted, (req, res) => {
         }) 
         .catch(error => {
             console.log(error);
-            res.send(error)
+            res.status(500).json({ message: 'Error getting all users'})
         })
 })
 
 // end point to delete user
-router.delete("/:id", (req,res) =>{
-    // check if we have the id firts
-    if(!req.params.id){
-       // if no id then return a 404, Not Found and error message
-       return res.status(404).json({errorMessage:"User with such id does not exist"})
-   }
-   Users.remove(req.params.id)
-       .then(response =>{
-           return res.status(200).json({message : "User has been deleted"})
-       })
-       .catch(error => {
-           console.log(error);
-           res.status(500).json("There is an error in the server while trying to delete the user")
-       })
-})
+router.delete("/:id", (req, res) => {
+    Users.remove(req.params.id)
+      .then(response => {
+          // if user was remove return http status code 200 Success, and message
+        if (response) {
+          res.status(200).json({ message: 'User has been response!' });
+        } 
+        // if not, then return a http status code 404 Not Found and message
+        else {
+          res.status(404).json({ message: 'User with such id does not exist!' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'There is an error in the server while trying to delete the user' });
+      });
+  });
+  
+// get a user by its specific id
+router.get("/:id", (req, res) => {
+    Users.findById(req.params.id)
+      .then(response => {
+        if (response) {
+          res.status(200).json(response);
+        } 
+        else {
+          res.status(404).json({ message: 'User with such id does not exist!' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'Error getting all users!' });
+      });
+  });
 
 module.exports = router;
